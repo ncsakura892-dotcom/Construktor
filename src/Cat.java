6f6b98466d59482920a58c7d60b0748e54029c3c
commit <ncsakura892@gmail.com>
2025-12-19 10:47:41 +0600

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+
+    String name;
+    int age;
+    String color;
+
+
+    public Cat (String name,int age,String color){
+
+        this.name=name;
+        this.age=age;
+        this.color=color;
+
+    }
+    public Cat (){}
+}
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        Dog dog=new Dog("REX",2,"Brown");
+        System.out.println("name:"+dog.name+
+                "\nage:"+dog.age+
+                "\nColor:"+dog.color);
+
+
+        Dog dog2=new Dog("Max",1,"Black");
+        System.out.println( );
+        System.out.println("name:"+dog2.name+
+                "\nage:"+dog2.age+
+                "\nColor:"+dog2.color);
+
+        Cat cat=new Cat("Luna",1,"Black");
+        System.out.println(" ");
+        System.out.println("name:"+cat.name+
+                "\nage:"+cat.age+
+                "\nColor:"+cat.color);
+
+
+        Cat cat2=new Cat("Mimu",5,"Brown");
+        System.out.println(" ");
+        System.out.println("name:"+cat2.name+
+                "\nage:"+cat2.age+
+                "\nColor:"+cat2.color);
+    }
+}
